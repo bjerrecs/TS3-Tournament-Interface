1bd97e2c44fa5f6279ad85c7a94f4ec0c87dc57c
bjerrecs <simonbjerrekristensen@gmail.com>
2018-11-09 15:59:34 +0100

    added feature: picture and setting for panel

--- index.php
+++ index.php
@@ -2,4 +2,10 @@
 
 require_once 'core.php';
 
-echo $twig->render('index.html', array('matches' => $config["matches"]));
+echo $twig->render(
+    'index.html', 
+    array(
+        'matches' => $config["matches"],
+        'info' => $config["panelinfo"]
+    )
+);
